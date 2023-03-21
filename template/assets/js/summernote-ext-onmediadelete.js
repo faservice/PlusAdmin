24569301d9f6db2dd5b6a1306cd3b9250968a18e
faservice <120707939+faservice@users.noreply.github.com>
2023-03-21 11:32:18 +0700

    Update summernote-ext-onmediadelete.js

--- template/assets/js/summernote-ext-onmediadelete.js
+++ template/assets/js/summernote-ext-onmediadelete.js
@@ -1,20 +1,36 @@
-$('#summernote').summernote({
-  onMediaDelete: function (src) {
-    // your code to remove the media file
-  },
-  toolbar: [
-    ['insert', ['picture', 'link', 'video']],
-    ['misc', ['onmediadelete']]
-  ],
-  buttons: {
-    onmediadelete: function (context) {
-      return $.summernote.ui.button({
-        contents: '<i class="fa fa-trash"/>',
-        tooltip: 'Delete media on selection',
-        click: function () {
-          context.invoke('onmediadelete.insert');
-        }
-      }).render();
-    }
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory(require('jquery'));
+  } else {
+    factory(window.jQuery);
   }
-});
+}(function ($) {
+  $.extend($.summernote.options, {
+    onMediaDelete: null
+  });
+
+  // add plugin
+  $.extend($.summernote.plugins, {
+    'onmediadelete': function (context) {
+      var self = this;
+
+      // add event listener to remove media when deleted
+      context.$note.on('summernote.keyup summernote.mouseup summernote.change', function () {
+        var $deletedMedia = context.$note.find('.note-editor [data-toggle="media"]');
+        if ($deletedMedia.length) {
+          $deletedMedia.each(function () {
+            var $media = $(this);
+            if (!$media.closest('.note-editor').length) {
+              $media.remove();
+              if ($.isFunction(context.options.onMediaDelete)) {
+                context.options.onMediaDelete($media.attr('src'));
+              }
+            }
+          });
+        }
+      });
+    }
+  }));
+}));
